65505903a102d7b827d6d769d1c25d29673cc37a
thebunnnny <thebunnnny@gmail.com>
2021-11-12 14:29:44 +1000

    Server example

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-setInterval(() => {
-  console.log(new Date().toLocaleTimeString());
-}, 1000);
+const http = require("http");
 
-console.log("Time is");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Home Page");
+  } else if (req.url === "/about") {
+    ///Blocking code (nested for loop)
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        console.log(`Var "i" is ${i} and var "j" is ${j}`);
+      }
+    }
+    res.end("About Us");
+  } else {
+    res.end(`
+    <h1 style="color:red;">Ooops</h1>
+    <h3>This page doesn't seem to exist...</h3>
+    `);
+  }
+});
+
+const port = 5000;
+server.listen(port, () => {
+  console.log(`Server is listening on Port: ${port}`);
+});
