fef799fa21fc84c8422009d49e90dcb46d99ef8f
thebunnnny <thebunnnny@gmail.com>
2021-11-15 15:28:14 +1000

    http streams

--- 18-http-stream.js
+++ 18-http-stream.js
@@ -4,11 +4,9 @@ const { readFileSync, createReadStream } = require("fs");
 console.log("stream example");
 
 const server = http.createServer((req, res) => {
-  ////ONE LARGE INSTANCE
   // const text = readFileSync("./content/big.txt", "utf8");
   // res.end(text);
 
-  ////IN CHUNKS
   const fileStream = createReadStream("./content/big.txt", "utf8");
   fileStream.on("open", () => {
     fileStream.pipe(res);
