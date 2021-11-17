2e02e24c3cb245794c5d76833a56babafcfddf28
thebunnnny <thebunnnny@gmail.com>
2021-11-17 15:36:19 +1000

    setting api manually with express

---
+++ 3-express-tutorial/final/4-all-static.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const { readFileSync, createReadStream } = require("fs");
+
+console.log("stream example");
+
+const server = http.createServer((req, res) => {
+  ////ONE LARGE INSTANCE
+  // const text = readFileSync("./content/big.txt", "utf8");
+  // res.end(text);
+
+  ////IN CHUNKS
+  const fileStream = createReadStream("./content/big.txt", "utf8");
+  fileStream.on("open", () => {
+    fileStream.pipe(res);
+  });
+  fileStream.on("error", err => {
+    res.end(err);
+  });
+});
+
+server.listen(5000);
