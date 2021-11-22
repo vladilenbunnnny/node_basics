0bb2482b2436f8365b91204a46f48fa8a23504ba
thebunnnny <thebunnnny@gmail.com>
2021-11-22 14:35:26 +1000

    post requests

---
+++ 3-express-tutorial/final/9-external-middleware-morgan.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const logger = require("./logger");
+const auth = require("./authorize");
+const morgan = require("morgan");
+
+// req => middleware => res
+
+//app.use(logger);
+//app.use([logger, auth]);
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("Items");
+});
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server on ${port}`);
+});
