964c70b51589238ca785a198d6127a574422f8c2
thebunnnny <thebunnnny@gmail.com>
2021-12-05 15:41:54 +1000

    pagination with limit per page query

--- 5-store-api/controllers/products.js
+++ 5-store-api/controllers/products.js
@@ -3,7 +3,8 @@ const ProductsModel = require("../models/products");
 const getAllProducts = async (req, res) => {
   // throw new Error("testing errors");
   console.log(";;;;;;;;;;;;;;----;;;;;;;;;;");
-  const { featured, company, name, sort, select } = req.query;
+
+  const { featured, company, name, sort, select, numericFilters } = req.query;
 
   const queryObject = {};
   if (featured) {
@@ -17,18 +18,31 @@ const getAllProducts = async (req, res) => {
   }
   //console.log(queryObject);
   let results = ProductsModel.find(queryObject);
+  ////SORTING
   if (sort) {
-    console.log(sort);
+    //console.log(sort);
     const sortList = sort.split(",").join(" ");
     results = results.sort(sortList);
   } else {
-    result = result.sort("createdAt");
+    results = results.sort("createdAt");
   }
+  ////SELECTING FIELDS
   if (select) {
     const selectList = select.split(",").join(" ");
     results = results.select(selectList);
   }
+  ////LIMIT and PAGE and SKIP
+  const limit = Number(req.query.limit) || 7;
+  const page = Number(req.query.page) || 1;
+  const skip = (page - 1) * limit;
+  results = results.skip(skip).limit(limit);
+
+  //RANGE
+  if (numericFilters) {
+    console.log(numericFilters);
+  }
   const products = await results;
+  //console.log(queryObject);
 
   res.status(200).json({ products, ngHits: products.length });
 };
