cf73259b22555d6bd45b0a3b22718efc3c5847e1
thebunnnny <thebunnnny@gmail.com>
2021-12-06 12:54:09 +1000

    testing basic JWT token

---
+++ 6-JWT-Basics/controllers/main.js
@@ -0,0 +1,10 @@
+const login = async (req, res) => {
+  res.send("Fake login");
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({ msg: `Hello, Name`, sexret: `Here is your authorized data, your lucky number is ${luckyNumber}` });
+};
+
+module.exports = { login, dashboard };
