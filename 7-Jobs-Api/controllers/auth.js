2c81d62fe1777e2f90f57d2f5e03dc6e387d82ac
thebunnnny <thebunnnny@gmail.com>
2021-12-14 20:24:52 +1000

    bcrypt js used to hash paswords with nongoose PRE save middleware

--- 7-Jobs-Api/controllers/auth.js
+++ 7-Jobs-Api/controllers/auth.js
@@ -4,20 +4,12 @@ const { BadRequestError } = require("../errors");
 const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const tempUser = { name, email, password: hashedPassword };
-  //console.log({ ...tempUser });
-
-  //console.log(name, email, password);
-  if (!name || !email || !password) {
-    throw new BadRequestError("Please, provide name, email and password");
+  try {
+    const user = await User.create({ ...req.body });
+    res.status(StatusCodes.CREATED).json({ user });
+  } catch (error) {
+    console.log(error);
   }
-  const user = await User.create({ ...tempUser });
-  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
--- 7-Jobs-Api/models/User.js
+++ 7-Jobs-Api/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -22,6 +23,12 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please, provide your password"],
     minlenght: 4,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
